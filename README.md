a52cb948706d4ea769d25ed0f22606d798425730
Lawrence Cherone <vripscript@gmail.com>
2018-02-01 05:24:03 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,57 @@
+## Domain Checker
+
+[![Build Status](https://travis-ci.org/phppackage/domain-check.svg?branch=master)](https://travis-ci.org/phppackage/domain-check)
+[![StyleCI](https://styleci.io/repos/REPO_ID_CHANGE_THIS/shield?branch=master)](https://styleci.io/repos/REPO_ID_CHANGE_THIS)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/phppackage/domain-check/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/phppackage/domain-check/?branch=master)
+[![Code Coverage](https://scrutinizer-ci.com/g/phppackage/domain-check/badges/coverage.png?b=master)](https://scrutinizer-ci.com/g/phppackage/domain-check/code-structure/master/code-coverage)
+[![Packagist Version](https://img.shields.io/packagist/v/phppackage/domain-check.svg?style=flat-square)](https://github.com/phppackage/domain-check/releases)
+[![Packagist Downloads](https://img.shields.io/packagist/dt/phppackage/domain-check.svg?style=flat-square)](https://packagist.org/packages/phppackage/domain-check)
+
+**WIP:** Domain availability checker.
+
+
+## Install
+
+Require this package with composer using the following command:
+
+``` bash
+$ composer require phppackage/domain-check
+```
+
+### Usage example:
+
+    <?php
+    require 'vendor/autoload.php';
+    
+    use \PHPPackage\domain-check\Checker;
+    
+    //
+    $checker = new Checker();
+    
+    /**
+     * Loolup all tlds for a given name, e.g domain.com, domain.co.uk ...
+     * @return array
+     */
+    $checker->availability('domain');
+    
+    
+
+## Testing
+
+``` bash
+$ composer test
+```
+
+## Contributing
+
+Please see [CONTRIBUTING](CONTRIBUTING.md) for details.
+
+
+## Credits
+
+ - [Lawrence Cherone](http://github.com/phppackage)
+ - [All Contributors](../../contributors)
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE) for more information.
---
+++ tests/DomainCheckTest.php
@@ -0,0 +1,265 @@
+<?php
+
+namespace PHPPackage\DomainCheck;
+
+use PHPUnit\Framework\TestCase;
+
+class DomainCheckTest extends TestCase
+{
+    use \phpmock\phpunit\PHPMock;
+
+    /**
+     * 
+     */
+    public function testObjectInstanceOf()
+    {
+        $this->assertInstanceOf(
+            'PHPPackage\DomainCheck\Checker',
+            new Checker()
+        );
+    }
+
+    /**
+     * 
+     */
+    public function testConstruct()
+    {
+        //
+        $this->assertClassHasAttribute('tlds', 'PHPPackage\DomainCheck\Checker');
+
+        //
+        $checker = new Checker(['uk']);
+
+        //
+        $this->assertInternalType('array', \PHPUnit\Framework\Assert::readAttribute($checker, 'tlds'));
+
+    }
+
+    /**
+     *
+     */
+    public function testWhoisServers()
+    {
+        $checker = new Checker();
+
+        $result = $checker->whoisServers(['com']);
+
+        $this->assertEquals(1, count($result));
+    }
+    
+    /**
+     *
+     */
+    public function testWhoisServersAll()
+    {
+        $checker = new Checker();
+
+        $result = $checker->whoisServers();
+
+        $this->assertEquals(true, (count($result) > 1000));
+    }
+
+    /**
+     *
+     */
+    public function testWhoisServersNotExists()
+    {
+        $checker = new Checker();
+
+        try {
+            $checker->whoisServers(['com'], '../tests/fixtures/not-exists-whois-servers.json');
+        } catch (\Exception $e) {
+            $this->assertInstanceOf('RuntimeException', $e);
+            $this->assertEquals('whois-servers.json does not exist or is not readable', $e->getMessage());
+        }
+    }
+    
+    /**
+     *
+     */
+    public function testWhoisServersEmpty()
+    {
+        $checker = new Checker();
+
+        try {
+            $checker->whoisServers(['com'], '../tests/fixtures/empty-whois-servers.json');
+        } catch (\Exception $e) {
+            $this->assertInstanceOf('RuntimeException', $e);
+            $this->assertEquals('invalid whois-servers.json file', $e->getMessage());
+        }
+    }
+
+    /**
+     *
+     */
+    public function testWhoisServersInvalid()
+    {
+        $checker = new Checker();
+
+        try {
+            $checker->whoisServers(['com'], '../tests/fixtures/invalid-whois-servers.json');
+        } catch (\Exception $e) {
+            $this->assertInstanceOf('RuntimeException', $e);
+            $this->assertEquals('invalid whois-servers.json file', $e->getMessage());
+        }
+    }
+    
+    /**
+     *
+     */
+    public function testAvailability()
+    {
+        //
+        $checker = new Checker();
+        
+        $this->assertTrue($checker->availability('foobar'));
+    }
+
+    /**
+     * 
+     */
+    public function testCheckDomainTrue()
+    {
+        //
+        $checker = new Checker();
+        
+        // open private method
+        $class = new \ReflectionClass($checker);
+        $method = $class->getMethod ('checkDomain');
+        $method->setAccessible(true);
+        
+        $fsockopen = $this->getFunctionMock(__NAMESPACE__, "fsockopen");
+        $fsockopen->expects($this->any())->willReturnCallback( // first time
+            function ($server, $port) {
+                $this->assertEquals("whois.server.test", $server);
+                $this->assertEquals(43, $port);
+                return true;
+            }
+        );
+
+        $fputs = $this->getFunctionMock(__NAMESPACE__, "fputs");
+        $fputs->expects($this->once())->willReturnCallback(
+            function ($socket, $data) {
+                $this->assertTrue($socket); // this comes from mock
+                $this->assertEquals("domain\r\n", $data);
+                return true;
+            }
+        );  
+        
+        $feof = $this->getFunctionMock(__NAMESPACE__, "feof");
+        $feof->expects($this->at(0))->willReturnCallback( // first loop
+            function ($socket) {
+                return false;
+            }
+        );
+        $feof->expects($this->at(1))->willReturnCallback( // second loop
+            function ($socket) {
+                return true;
+            }
+        );
+        
+        $fgets = $this->getFunctionMock(__NAMESPACE__, "fgets");
+        $fgets->expects($this->any())->willReturnCallback(
+            function ($socket, $length) {
+                $this->assertEquals(256, $length);
+                return 'not found';
+            }
+        );  
+        
+        $fclose = $this->getFunctionMock(__NAMESPACE__, "fclose");
+        $fclose->expects($this->once())->willReturnCallback(
+            function ($socket) {
+                $this->assertTrue($socket);
+            }
+        );  
+        
+        $this->assertTrue($method->invoke($checker, 'domain', 'whois.server.test', 'not found'));
+    }
+    
+    /**
+     * 
+     */
+    public function testCheckDomainFalseDomainTaken()
+    {
+        //
+        $checker = new Checker();
+        
+        // open private method
+        $class = new \ReflectionClass($checker);
+        $method = $class->getMethod ('checkDomain');
+        $method->setAccessible(true);
+        
+        $fsockopen = $this->getFunctionMock(__NAMESPACE__, "fsockopen");
+        $fsockopen->expects($this->once())->willReturnCallback( // first time
+            function ($server, $port) {
+                $this->assertEquals("whois.server.test", $server);
+                $this->assertEquals(43, $port);
+                return true;
+            }
+        );
+
+        $fputs = $this->getFunctionMock(__NAMESPACE__, "fputs");
+        $fputs->expects($this->once())->willReturnCallback(
+            function ($socket, $data) {
+                $this->assertTrue($socket); // this comes from mock
+                $this->assertEquals("domain\r\n", $data);
+                return true;
+            }
+        );  
+        
+        $feof = $this->getFunctionMock(__NAMESPACE__, "feof");
+        $feof->expects($this->at(0))->willReturnCallback( // first loop
+            function ($socket) {
+                return false;
+            }
+        );
+        $feof->expects($this->at(1))->willReturnCallback( // second loop
+            function ($socket) {
+                return true;
+            }
+        );
+        
+        $fgets = $this->getFunctionMock(__NAMESPACE__, "fgets");
+        $fgets->expects($this->any())->willReturnCallback(
+            function ($socket, $length) {
+                $this->assertEquals(256, $length);
+                return 'Large whois response';
+            }
+        );  
+        
+        $fclose = $this->getFunctionMock(__NAMESPACE__, "fclose");
+        $fclose->expects($this->once())->willReturnCallback(
+            function ($socket) {
+                $this->assertTrue($socket);
+            }
+        );  
+        
+        $this->assertFalse($method->invoke($checker, 'domain', 'whois.server.test', 'not found'));
+    }
+
+    /**
+     * 
+     */
+    public function testCheckDomainFalseNoConnection()
+    {
+        //
+        $checker = new Checker();
+        
+        // open private method
+        $class = new \ReflectionClass($checker);
+        $method = $class->getMethod ('checkDomain');
+        $method->setAccessible(true);
+        
+        $fsockopen = $this->getFunctionMock(__NAMESPACE__, "fsockopen");
+        $fsockopen->expects($this->any())->willReturnCallback(
+            function ($server, $port) {
+                $this->assertEquals("whois.server.test", $server);
+                $this->assertEquals(43, $port);
+                return false;
+            }
+        );
+
+        $this->assertFalse($method->invoke($checker, 'domain', 'whois.server.test', 'not found'));
+    }
+
+}
